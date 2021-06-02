4f55722006e4369f937e473eeb4b6324c7c909c2
Thomas Barratt <tb@thomasbarratt.co.uk>
2021-06-02 11:17:01 +0200

    [FORMAT] Bigger title for homepage

--- components/intro.js
+++ components/intro.js
@@ -6,8 +6,9 @@ export default function Intro({ title, content, alwaysShowTitle }) {
     <Header 
       title={title} 
       subtitle={content} 
-      showTriangle={true} 
+      showTriangle 
       alwaysShowTitle={alwaysShowTitle}
+      bigTitle
     />
     // <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
     //   <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
