06e7be8e83fa23e1da8ac0957effaa75924265d9
Thomas Barratt <tb@thomasbarratt.co.uk>
2021-09-19 17:06:33 +0200

    [FORMAT] Add icons to site

--- components/meta.js
+++ components/meta.js
@@ -4,33 +4,12 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#03A9F4"/>
+      <meta name="msapplication-TileColor" content="#03A9F4"/>
+      <meta name="theme-color" content="#03A9F4"/>
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
